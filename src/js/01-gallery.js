b9e88eeba861f36862504fd55a9e1dfb57c62018
DmitryMeleshko <meleshko.triolcorp@gmail.com>
2023-02-12 22:57:36 +0200

    Модуль 7, завдання 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const markup = createImgMarkup(galleryItems);
 
+galleryContainer.insertAdjacentHTML('afterbegin', markup);
 
-const galleryContainer = document.querySelector('.gallery');
-const markup = galleryItems
-.map(({preview, original, description}) => {
+galleryContainer.addEventListener('click', (event) => {;
+
+function imgClick (event){
+    event.preventDefault();
+    if(!event.target.nodeName !== "IMG") 
+    return;
+ }
+ console.log("target", event.target);
+ console.log("currentTarget", event.currentTarget);
+
+ const imageBigSize = event.target.getAttribute("data-source");
+
+ const instance = basicLightbox.create(`<img src = "${imageBigSize}">`);
+
+ instance.show();
+});
+
+function createImgMarkup (galleryItems){return galleryItems.map(({preview, original, description}) => {
     return `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" title="$description"/>
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" 
+            title="$description" width="340">
+            </img>
         </a> 
     </li>`;
-}) .join("");
+}) .join("")
+};
 
-galleryContainer.innerHTML = markup;
-let lightbox = new SimpleLightbox(".gallery a", {captionDelay:250,}, {captionsData: "alt",});
 
-const imgClick = (e) =>{
-if(e.target.nodeName !== 'IMG') {
- }
-}
-galleryContainer.addEventListener("click", imgClick);
+
+
+
